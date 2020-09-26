9293b79c4e002dc52184c3c2d69fe8a74b2f03d9
David Kirakosyan <david.kirakosyan.41@mail.ru>
2020-09-26 23:38:09 +0400

    in 3_2picture.py added faces, hairs. middle ice cream changed.

--- Lab 4 (Pygame part 1)/3_2picture.py
+++ Lab 4 (Pygame part 1)/3_2picture.py
@@ -6,6 +6,19 @@ import math
 def man(plot, x, y, width, height):
     ellipse(plot, COLOR['PURPLE'], (x, y, width, height))
     circle(plot, COLOR['FACE_COLOR'], (x + width // 2, y - height // 6), height//5)
+    # eyes
+    circle(plot, COLOR['WHITE'], (x+width//3, y-int(0.22*height)), height//20)
+    circle(plot, COLOR['BLACK'], (x+int(0.32*width), y-int(0.22*height)), height//45)
+    circle(plot, COLOR['WHITE'], (x+int(0.65*width), y-int(0.22*height)), height//20)
+    circle(plot, COLOR['BLACK'], (x+int(0.66*width), y-int(0.22*height)), height//45)
+    # nose
+    line(plot, COLOR['BLACK'],
+            (x+width//2, y-int(0.2*height)),
+            (x+width//2, y-int(0.1*height)), 1)
+    # mouth
+    arc(plot, COLOR['BLACK'],
+        (x+int(0.35*width), y-int(0.18*height), int(0.3*width), height//6),
+        1.1*math.pi, 1.9*math.pi, 2)
     # hands
     line(plot, COLOR['BLACK'],
          (x + width//5, y + height//10),
@@ -29,10 +42,25 @@ def man(plot, x, y, width, height):
 
 
 def woman(plot, x, y, width, height):
-    # woman
+    # hairs
+    circle(plot, COLOR['BROWN'], (x, y-height//8), height//4)
+    rect(plot, COLOR['BROWN'], (x-height//4, y-height//8, height//2, height//2))
     polygon(plot, COLOR['PINK'],
             [(x, y), (x-width//2, y+height), (x+width//2, y+height)])
     circle(plot, COLOR['FACE_COLOR'], (x, y-height//16), 2*height//11)
+    # eyes
+    circle(plot, COLOR['WHITE'], (x-int(0.1*width), y-int(0.1*height)), height//25)
+    circle(plot, COLOR['BLACK'], (x-int(0.1*width), y-int(0.1*height)), height//50)
+    circle(plot, COLOR['WHITE'], (x+int(0.1*width), y-int(0.1*height)), height//25)
+    circle(plot, COLOR['BLACK'], (x+int(0.1*width), y-int(0.1*height)), height//50)
+    # nose
+    line(plot, COLOR['BLACK'],
+         (x, y - int(0.1*height)),
+         (x, y), 1)
+    # mouth
+    arc(plot, COLOR['BLACK'],
+        (x-int(0.1*width), y-int(0.08*height), int(0.2*width), height//6),
+        1.1 * math.pi, 1.9 * math.pi, 2)
     # hands
     line(plot, COLOR['BLACK'],
             (x - width//12, y + height//6),
@@ -58,15 +86,15 @@ def woman(plot, x, y, width, height):
             (x + int(0.3*width), y + int(1.45*height) + 3), 2)
 
 
-def balloons(plot, x, y, angle):
-    x2 = int(x - 80*math.cos(math.radians(angle)))
-    y2 = int(y - 80*math.sin(math.radians(angle)))
-    x3 = int(x + 80*math.cos(math.radians(180 - angle - 60)))
-    y3 = int(y - 80*math.sin(math.radians(180 - angle - 60)))
+def balloons(plot, x, y, angle, size=80):
+    x2 = int(x - size * math.cos(math.radians(angle)))
+    y2 = int(y - size * math.sin(math.radians(angle)))
+    x3 = int(x + size * math.cos(math.radians(180 - angle - 60)))
+    y3 = int(y - size * math.sin(math.radians(180 - angle - 60)))
     polygon(screen, COLOR['RED'],
             [(x, y), (x2, y2), (x3, y3)])
-    circle(plot, COLOR['RED'], (int(0.25*x2 + 0.75*x3), int(0.25*y2 + 0.75*y3)), 22)
-    circle(plot, COLOR['RED'], (int(0.75*x2 + 0.25*x3), int(0.75*y2 + 0.25*y3)), 22)
+    circle(plot, COLOR['RED'], (int(0.25*x2 + 0.75*x3), int(0.25*y2 + 0.75*y3)), size//4)
+    circle(plot, COLOR['RED'], (int(0.75*x2 + 0.25*x3), int(0.75*y2 + 0.25*y3)), size//4)
 
 
 def ice_cream(plot, x, y, angle, size):
@@ -117,7 +145,7 @@ line(screen, COLOR['BLACK'], (80, 510), (65, 300), 2)
 balloons(screen, 65, 300, 50)
 
 line(screen, COLOR['BLACK'], (w//2, 30+h//2), (20+w//2, h//4), 2)
-ice_cream(screen, 20+w//2, h//4, 60, size=100)
+balloons(screen, 20+w//2, h//4, 60, size=120)
 
 ice_cream(screen, 940, 500, 90, size=80)
 
