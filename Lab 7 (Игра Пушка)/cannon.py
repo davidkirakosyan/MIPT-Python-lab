15353e391f1ef1879f2ae7f6e3415657ea86a695
David Kirakosyan <david.kirakosyan.41@mail.ru>
2020-10-26 15:41:52 +0400

    Little changes in cannon.py
    Game.mainloop method refactored, created Game.show_scores, Game.show_tries, Game.handle_missiles methods.
    Missiles are now located in cannon.missiles array. It is possible to shoot many times. Missile color changed. Friction with ground added.

--- Lab 7 (Игра Пушка)/cannon.py
+++ Lab 7 (Игра Пушка)/cannon.py
@@ -5,6 +5,9 @@ import math
 
 
 class Game:
+    """
+    Controls game
+    """
     FPS = 30
     score = 0
     COLORS = {
@@ -14,8 +17,8 @@ class Game:
         'GREEN': 0x00FF00,
         'MAGENTA': 0xFF00FF,
         'CYAN': 0x00FFFF,
-        'BLACK': 0x0,
     }
+    BLACK = (0, 0, 0)
 
     def __init__(self, width, height):
         self.width = width
@@ -26,6 +29,7 @@ class Game:
         self.screen = pygame.display.set_mode((self.width, self.height))
         self.screen.fill(self.bg)
         self.clock = pygame.time.Clock()
+
         self.cannon = Cannon(
             self.screen,
             0.1 * self.width,
@@ -33,12 +37,14 @@ class Game:
             0.05 * self.height,
             random.choice(list(self.COLORS.values())),
         )
-        self.missile = None
+
+        target_colors = list(self.COLORS.values())
+        target_colors.remove(self.cannon.color)
         self.target = Ball(
             random.randint(7 * self.width // 10, self.width),
             random.randint(self.height // 10, 8 * self.height // 10),
-            random.randint(1, self.height // 20),
-            random.choice(list(self.COLORS.values())),
+            random.randint(self.height // 30, self.height // 20),
+            random.choice(target_colors),
         )
 
     def mainloop(self):
@@ -48,7 +54,7 @@ class Game:
             self.clock.tick(self.FPS)
 
             self.screen.fill(self.bg)
-            line(self.screen, self.COLORS['BLACK'], (0, 9 * self.height // 10),
+            line(self.screen, self.BLACK, (0, 9 * self.height // 10),
                  (self.width, 9 * self.height // 10))
 
             for event in pygame.event.get():
@@ -59,24 +65,61 @@ class Game:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_down = True
                 if event.type == pygame.MOUSEBUTTONUP:
+                    self.cannon.shoot()
+                    self.target.tries += 1
                     mouse_down = False
-                    self.cannon.shoot(self)
             if mouse_down:
                 self.cannon.increase_muzzle()
 
-            if self.missile:
-                y_max = 0.9 * self.height
-                x_max = self.width
-                self.missile.move(x_max, y_max)
-                self.missile.draw(self)
-                if self.missile.is_dead(y_max):
-                    self.missile = None
+            if self.cannon.missiles:
+                self.handle_missiles()
 
+            self.show_scores()
             self.target.draw(self)
-
             self.cannon.draw(self)
             pygame.display.update()
         pygame.quit()
+
+    def show_scores(self):
+        font = pygame.font.SysFont('arial', 25, True)
+        text = font.render("Score: {}".format(self.score), True, self.BLACK)
+        x_ = self.width // 20
+        y_ = self.height // 10
+        self.screen.blit(text, (x_, y_))
+
+    def show_tries(self, other):
+        """
+        :type other: Target
+        """
+        self.screen.fill(self.bg)  # render only text
+
+        font = pygame.font.SysFont('arial', 30, True)
+        text = font.render("Target was shot after {} tries.".format(other.tries), True, self.BLACK)
+        rect_ = text.get_rect(center=(self.width // 2, self.height // 2))
+        self.screen.blit(text, rect_)
+
+        pygame.display.update()
+        pygame.time.delay(1500)  # 1.5 sec
+
+    def handle_missiles(self):
+        y_max = 0.9 * self.height
+        x_max = self.width
+        for missile in self.cannon.missiles:
+            if missile.is_dead(y_max):
+                self.cannon.missiles.remove(missile)
+            elif self.target.is_touching(missile):
+                self.cannon.missiles = []
+                self.show_tries(self.target)
+
+                self.target = Ball(
+                    random.randint(5 * self.width // 10, 8 * self.width // 10),
+                    random.randint(3 * self.height // 10, 8 * self.height // 10),
+                    random.randint(self.height // 30, self.height // 20),
+                    random.choice(list(self.COLORS.values())),
+                )
+                self.score += 1
+            missile.move(x_max, y_max)
+            missile.draw(self)
 
 
 class Cannon:
@@ -96,6 +139,7 @@ class Cannon:
         self.width = 2 * self.r
         self.height = self.width // 4
         self.delta_muz = self.r // 25
+        self.missiles = []
 
     def draw(self, other: Game):
         """
@@ -103,7 +147,7 @@ class Cannon:
         :return: None
         """
         circle(self.screen, self.color, (self.x, self.y), self.r)
-        circle(self.screen, other.COLORS['BLACK'], (self.x, self.y), self.r, 2)
+        circle(self.screen, other.BLACK, (self.x, self.y), self.r, 2)
 
         plot = pygame.Surface((self.width, self.height))
         plot.fill(self.color)
@@ -131,31 +175,30 @@ class Cannon:
             self.delta_muz *= -1
         self.width += self.delta_muz
 
-    def shoot(self, other: Game):
+    def shoot(self):
+        self.missiles.append(Missile(
+            self.screen,
+            self.x + self.width * math.cos(self.angle),
+            self.y - self.width * math.sin(self.angle),
+            self.color,
+            self.r // 2,
+            self.angle,
+            20 * (self.width / self.r - 1)
+        ))
         self.width = 2 * self.r
-        if not other.missile:
-            other.missile = Missile(
-                self.screen,
-                self.x + self.width * math.cos(self.angle),
-                self.y - self.width * math.sin(self.angle),
-                other.COLORS['BLACK'],
-                self.r // 2,
-                self.angle,
-                35 * (self.width / self.r - 1)
-            )
 
 
 class Missile:
-    g = 2
-    k = 0.01
+    g = 10
+    k = 0.1
 
     def __init__(self, screen, x, y, color, radius, angle, vel):
         self.screen = screen
         self.x = int(x)
         self.y = int(y)
-        self.angle = int(angle)
-        self.v_x = int(vel * math.cos(self.angle))
-        self.v_y = - int(vel * math.sin(self.angle))
+        self.angle = angle
+        self.v_x = vel * math.cos(self.angle)
+        self.v_y = - vel * math.sin(self.angle)
         self.r = int(radius)
         self.color = color
         self.time = 0
@@ -168,15 +211,17 @@ class Missile:
         :param y_max: screen's height
         :return:
         """
-        dt = 0.5
-        if self.x <= self.r or x_max - self.x <= 1.5 * self.r:
-            self.v_x *= -1
-        if self.y <= self.r or y_max - self.y <= 1.5 * self.r:
-            self.v_y *= -1
-        self.v_x += - int(self.k * self.v_x) * dt
-        self.v_y += self.g - int(self.k * self.v_y) * dt
-        self.x += int(self.v_x * dt)
-        self.y += int(self.v_y * dt)
+        dt = 0.07
+        if self.x <= self.r or x_max - self.x <= 2 * self.r:
+            self.v_x *= -0.9
+        if y_max - self.y <= 2 * self.r:
+            self.v_y *= -0.5
+            self.v_x *= 0.8  # ground friction
+        self.v_x += - self.k * self.v_x * dt
+        self.v_y += self.g * dt - self.k * self.v_y * dt
+
+        self.x = int(self.x + self.v_x)
+        self.y = int(self.y + self.v_y)
 
         self.time += dt
 
@@ -189,8 +234,10 @@ class Missile:
         circle(self.screen, self.color, (self.x, self.y), self.r)
 
     def is_dead(self, y_max: int):
-        is_on_bottom = y_max - self.y <= 0.5 * self.r
-        return self.time > 120 or is_on_bottom
+        is_on_bottom = y_max - self.y <= 2 * self.r and abs(self.v_x) <= 2
+        is_over_top = self.y <= 0
+        return self.time > 100 or is_on_bottom or is_over_top
+
 
 class Target:
     def __init__(self, x, y, size, color):
@@ -198,6 +245,7 @@ class Target:
         self.y = int(y)
         self.size = int(size)
         self.color = color
+        self.tries = 0
 
 
 class Ball(Target):
@@ -207,7 +255,7 @@ class Ball(Target):
         :return: None
         """
         circle(other.screen, self.color, (self.x, self.y), self.size)
-        circle(other.screen, other.COLORS['BLACK'], (self.x, self.y), self.size, 1)
+        circle(other.screen, other.BLACK, (self.x, self.y), self.size, 1)
 
     def is_touching(self, other: Missile):
         """
